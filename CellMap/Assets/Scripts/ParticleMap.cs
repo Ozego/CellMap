0172a0fcb1f14dac61fa08f98846907d2e51cbf3
Esben Jacob Sloth <esben@trueaxiongames.com>
2019-09-20 18:00:03 +0700

    particle buffer

--- CellMap/Assets/Scripts/ParticleMap.cs
+++ CellMap/Assets/Scripts/ParticleMap.cs
@@ -1,18 +1,63 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ParticleMap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Vector2Int size = new Vector2Int(256,256);
+    [SerializeField] private int cellSize = 8;
+    [SerializeField] private ComputeShader pShader;
+    [SerializeField] bool evaluateDebugFunction;
+
+    void OnValidate()
     {
-        
+        if(evaluateDebugFunction)
+        {
+            evaluateDebugFunction = false;
+            dFunc();
+        }
     }
 
-    // Update is called once per frame
+    private void dFunc()
+    {
+        var tParticle = new particle();
+        tParticle._position._x = 5f;
+        tParticle._position._y = 53f;
+        Debug.Log( tParticle._position );
+    }
+
+    void OnDrawGizmos()
+    {
+    }
+    void Start()
+    {
+    }
     void Update()
     {
-        
+    }
+
+
+    public struct particle
+    {
+        public float2 _position;
+        public float2 _direction;
+        public float2 _size;
+        public int byteSize
+        { 
+            get => 
+                  _position.byteSize
+                + _direction.byteSize
+                + _size.byteSize ; 
+        }
+    }
+    public struct float2
+    {
+        public float _x;
+        public float _y;
+        public int byteSize 
+        { 
+            get => sizeof(float) * 2 ; 
+        }
     }
 }

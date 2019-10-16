39341013a9f2bac46fb28ff1da30a8e1af9681e1
Esben Jacob Sloth <esben@trueaxiongames.com>
2019-10-16 16:19:17 +0700

    preset functionality save/load functionality line plot method selector

--- CellMap/Assets/Scripts/LinePlotMap.cs
+++ CellMap/Assets/Scripts/LinePlotMap.cs
@@ -13,7 +13,7 @@ public class LinePlotMap : MonoBehaviour
     [SerializeField]    private     Boolean     RandomizeAttributes             = false;
     [Tooltip("Size of the bitmap the lines are plotted on. \nMap size is floored to a multiple of 8")]
     [SerializeField]    private     Vector2Int  size;
-    [Tooltip("Size of the hidden jittered grid the initiating lines are plotted on.")]
+    [SerializeField]    private     plotMethods plotMethod;
     [SerializeField]    private     int         gridSize                        = 256 ;
     [SerializeField]    private     float       initialSpeed                    = 2f;
     [SerializeField]    private     float       initialDirection                = 0f;
@@ -39,6 +39,9 @@ public class LinePlotMap : MonoBehaviour
     [SerializeField]    private     bool        spawnSymmetrically              = false ;
     [Header("Required Components")]
     [SerializeField]    private     ComputeShader   csErrosion;
+    [Header("Droplets")]
+    [SerializeField]    private     TextAsset       loadFile;
+
 
                         private     RenderTexture   lineMapTexture;
                         private     MeshRenderer    displayRenderer;
@@ -47,6 +50,7 @@ public class LinePlotMap : MonoBehaviour
                         private     int             maxHeads;
                         private     int ConsumeBufferID = 0, 
                                         AppendBufferID  = 1;
+                        private     SaveState       presentSaveState            = new SaveState();
     //Structs
     public struct Head
     {
@@ -58,6 +62,12 @@ public class LinePlotMap : MonoBehaviour
         
         static public int ByteSize{get{return sizeof(float)*6+sizeof(int);}}
     }
+    private enum plotMethods
+    {
+        GetHead,
+        GetHeadsOnLine,
+        GetHeadsOnGrid
+    }
     //Editor
     void OnGizmos()
     {
@@ -67,7 +77,35 @@ public class LinePlotMap : MonoBehaviour
         size.x     = size.x     & 0xFFF8; //if( size.x % 8 != 0 ) size.x = Mathf.CeilToInt( (float)size.x / 8f ) * 8;
         size.y     = size.y     & 0xFFF8; //if( size.y % 8 != 0 ) size.y = Mathf.CeilToInt( (float)size.y / 8f ) * 8;
         frameCount = frameCount & 0xFFFE; 
+        if(loadFile!=null)
+        {
+            LoadAttributes();
+            loadFile = null;
+        }
     }
+
+    private void LoadAttributes()
+    {
+        var loadState = JsonUtility.FromJson<SaveState>(loadFile.text);
+        gridSize                                        = loadState.gridSize;
+        initialSpeed                                    = loadState.initialSpeed;
+        initialDirection                                = loadState.initialDirection;
+        initialDirectionVariation                       = loadState.initialDirectionVariation;
+        lineSpawnChance                                 = 1f - loadState.lineSpawnChance;
+        lineAcceleration                                = loadState.lineAcceleration;
+        lineSpawnAcceleration                           = loadState.lineSpawnAcceleration;
+        lineVariation                                   = loadState.lineVariation;
+        lineSpawnVariation                              = loadState.lineSpawnVariation;
+        lineAngularAcceleration                         = loadState.lineAngularAcceleration;
+        lineSpawnAngularAcceleration                    = loadState.lineSpawnAngularAcceleration;
+        lineAngularVariation                            = loadState.lineAngularVariation;
+        lineSpawnAngularVariation                       = loadState.lineSpawnAngularVariation;
+        spawnAngle                                      = loadState.spawnAngle;
+        spawnAngleVariation                             = loadState.spawnAngleVariation;
+        int b1                                          = 0b0000_0000_0000_0001; 
+        spawnSymmetrically                              = ( loadState.boolArray & b1 ) == b1;
+    }
+
     //Game
     void Awake()
     {
@@ -118,7 +156,7 @@ public class LinePlotMap : MonoBehaviour
     {
         ClearMap();
         SetShaderAttributes();
-        int GetHeadsKID = csErrosion.FindKernel("GetHeads");
+        int GetHeadsKID = csErrosion.FindKernel(plotMethod.ToString());
         csErrosion.Dispatch(GetHeadsKID, size.x / 8, size.y / 8, 1);
     }
 
@@ -137,53 +175,94 @@ public class LinePlotMap : MonoBehaviour
 
     private void SetShaderAttributes()
     {
-        if(RandomizeAttributes)
-        {
-            SetRandomShaderAttributes();
-            return;
-        }
-        csErrosion.SetInt(   "gridSize",                        gridSize);
-        csErrosion.SetFloat( "initialSpeed",                    initialSpeed);
-        csErrosion.SetFloat( "initialDirection",                initialDirection);
-        csErrosion.SetFloat( "initialDirectionVariation",       initialDirectionVariation);
-        csErrosion.SetFloat( "lineSpawnChance",                 1f - lineSpawnChance);
-        csErrosion.SetFloat( "lineAcceleration",                lineAcceleration);
-        csErrosion.SetFloat( "lineSpawnAcceleration",           lineSpawnAcceleration);
-        csErrosion.SetFloat( "lineVariation",                   lineVariation);
-        csErrosion.SetFloat( "lineSpawnVariation",              lineSpawnVariation);
-        csErrosion.SetFloat( "lineAngularAcceleration",         lineAngularAcceleration);
-        csErrosion.SetFloat( "lineSpawnAngularAcceleration",    lineSpawnAngularAcceleration);
-        csErrosion.SetFloat( "lineAngularVariation",            lineAngularVariation);
-        csErrosion.SetFloat( "lineSpawnAngularVariation",       lineSpawnAngularVariation);
-        csErrosion.SetFloat( "spawnAngle",                      spawnAngle);
-        csErrosion.SetFloat( "spawnAngleVariation",             spawnAngleVariation);
-        int boolArray                        = 0b0000_0000_0000_0000;
-        if(spawnSymmetrically) boolArray    |= 0b0000_0000_0000_0001;
-        csErrosion.SetInt(   "boolArray",                       boolArray);
+        SetSaveStateAttributes();
+        csErrosion.SetInt(   "gridSize",                        presentSaveState.gridSize );
+        csErrosion.SetFloat( "initialSpeed",                    presentSaveState.initialSpeed );
+        csErrosion.SetFloat( "initialDirection",                presentSaveState.initialDirection );
+        csErrosion.SetFloat( "initialDirectionVariation",       presentSaveState.initialDirectionVariation );
+        csErrosion.SetFloat( "lineSpawnChance",                 presentSaveState.lineSpawnChance );
+        csErrosion.SetFloat( "lineAcceleration",                presentSaveState.lineAcceleration );
+        csErrosion.SetFloat( "lineSpawnAcceleration",           presentSaveState.lineSpawnAcceleration );
+        csErrosion.SetFloat( "lineVariation",                   presentSaveState.lineVariation );
+        csErrosion.SetFloat( "lineSpawnVariation",              presentSaveState.lineSpawnVariation );
+        csErrosion.SetFloat( "lineAngularAcceleration",         presentSaveState.lineAngularAcceleration );
+        csErrosion.SetFloat( "lineSpawnAngularAcceleration",    presentSaveState.lineSpawnAngularAcceleration );
+        csErrosion.SetFloat( "lineAngularVariation",            presentSaveState.lineAngularVariation );
+        csErrosion.SetFloat( "lineSpawnAngularVariation",       presentSaveState.lineSpawnAngularVariation );
+        csErrosion.SetFloat( "spawnAngle",                      presentSaveState.spawnAngle );
+        csErrosion.SetFloat( "spawnAngleVariation",             presentSaveState.spawnAngleVariation );
+        csErrosion.SetInt(   "boolArray",                       presentSaveState.boolArray );
     }
     
-    private void SetRandomShaderAttributes()
+    private void SetSaveStateAttributes()
     {
-        csErrosion.SetInt(   "gridSize",                        Random.Range(0, gridSize*2));
-        csErrosion.SetFloat( "initialSpeed",                    Random.Range(0, initialSpeed*2f));
-        csErrosion.SetFloat( "initialDirection",                Random.Range(0, initialDirection*2f));
-        csErrosion.SetFloat( "initialDirectionVariation",       Random.Range(0, initialDirectionVariation*2f));
-        csErrosion.SetFloat( "lineSpawnChance",                 1f - Random.value * lineSpawnChance);
-        csErrosion.SetFloat( "lineAcceleration",                Random.Range(-1f, 1f)*Mathf.Abs(1f-lineAcceleration)+1f);
-        csErrosion.SetFloat( "lineSpawnAcceleration",           Random.Range(-1f, 1f)*Mathf.Abs(1f-lineSpawnAcceleration)+1f);
-        csErrosion.SetFloat( "lineVariation",                   Random.Range(0, lineVariation*2f));
-        csErrosion.SetFloat( "lineSpawnVariation",              Random.Range(0, lineSpawnVariation*2f));
-        csErrosion.SetFloat( "lineAngularAcceleration",         Random.Range(-1f, 1f)*Mathf.Abs(1f-lineAngularAcceleration)+1f);
-        csErrosion.SetFloat( "lineSpawnAngularAcceleration",    Random.Range(-1f, 1f)*Mathf.Abs(1f-lineSpawnAngularAcceleration)+1f);
-        csErrosion.SetFloat( "lineAngularVariation",            Random.Range(0, lineAngularVariation*2f));
-        csErrosion.SetFloat( "lineSpawnAngularVariation",       Random.Range(0, lineSpawnAngularVariation*2f));
-        csErrosion.SetFloat( "spawnAngle",                      Random.Range(0, spawnAngle*2f));
-        csErrosion.SetFloat( "spawnAngleVariation",             Random.Range(0, spawnAngleVariation*2f));
-        int boolArray                                           =  0b0000_0000_0000_0000;
-        if(Random.value > 0.5f & spawnSymmetrically) boolArray  |= 0b0000_0000_0000_0001;
-        csErrosion.SetInt(   "boolArray",                       boolArray);
+        if(RandomizeAttributes)
+        {
+            SetRandomSaveStateAttributes();
+            return;
+        }
+        presentSaveState.gridSize                       = gridSize;
+        presentSaveState.initialSpeed                   = initialSpeed;
+        presentSaveState.initialDirection               = initialDirection;
+        presentSaveState.initialDirectionVariation      = initialDirectionVariation;
+        presentSaveState.lineSpawnChance                = 1f - lineSpawnChance;
+        presentSaveState.lineAcceleration               = lineAcceleration;
+        presentSaveState.lineSpawnAcceleration          = lineSpawnAcceleration;
+        presentSaveState.lineVariation                  = lineVariation;
+        presentSaveState.lineSpawnVariation             = lineSpawnVariation;
+        presentSaveState.lineAngularAcceleration        = lineAngularAcceleration;
+        presentSaveState.lineSpawnAngularAcceleration   = lineSpawnAngularAcceleration;
+        presentSaveState.lineAngularVariation           = lineAngularVariation;
+        presentSaveState.lineSpawnAngularVariation      = lineSpawnAngularVariation;
+        presentSaveState.spawnAngle                     = spawnAngle;
+        presentSaveState.spawnAngleVariation            = spawnAngleVariation;
+        int boolArray                                   = 0b0000_0000_0000_0000;
+        if(spawnSymmetrically) boolArray               |= 0b0000_0000_0000_0001;
+        presentSaveState.boolArray                      = boolArray;
     }
-
+    
+    private void SetRandomSaveStateAttributes()
+    {
+        presentSaveState.gridSize                       = Random.Range(0, gridSize*2);
+        presentSaveState.initialSpeed                   = Random.Range(0, initialSpeed*2f);
+        presentSaveState.initialDirection               = Random.Range(0, initialDirection*2f);
+        presentSaveState.initialDirectionVariation      = Random.Range(0, initialDirectionVariation*2f);
+        presentSaveState.lineSpawnChance                = 1f - Random.value * lineSpawnChance;
+        presentSaveState.lineAcceleration               = Random.Range(-1f, 1f)*Mathf.Abs(1f-lineAcceleration)+1f;
+        presentSaveState.lineSpawnAcceleration          = Random.Range(-1f, 1f)*Mathf.Abs(1f-lineSpawnAcceleration)+1f;
+        presentSaveState.lineVariation                  = Random.Range(0, lineVariation*2f);
+        presentSaveState.lineSpawnVariation             = Random.Range(0, lineSpawnVariation*2f);
+        presentSaveState.lineAngularAcceleration        = Random.Range(-1f, 1f)*Mathf.Abs(1f-lineAngularAcceleration)+1f;
+        presentSaveState.lineSpawnAngularAcceleration   = Random.Range(-1f, 1f)*Mathf.Abs(1f-lineSpawnAngularAcceleration)+1f;
+        presentSaveState.lineAngularVariation           = Random.Range(0, lineAngularVariation*2f);
+        presentSaveState.lineSpawnAngularVariation      = Random.Range(0, lineSpawnAngularVariation*2f);
+        presentSaveState.spawnAngle                     = Random.Range(0, spawnAngle*2f);
+        presentSaveState.spawnAngleVariation            = Random.Range(0, spawnAngleVariation*2f);
+        int boolArray                                   = 0b0000_0000_0000_0000;
+        if(Random.value > 0.5f & spawnSymmetrically) 
+            boolArray                                  |= 0b0000_0000_0000_0001;
+        presentSaveState.boolArray                      = boolArray;
+    }
+    private class SaveState
+    {
+        public int     gridSize;
+        public float   initialSpeed;
+        public float   initialDirection;
+        public float   initialDirectionVariation;
+        public float   lineSpawnChance;
+        public float   lineAcceleration;
+        public float   lineSpawnAcceleration;
+        public float   lineVariation;
+        public float   lineSpawnVariation;
+        public float   lineAngularAcceleration;
+        public float   lineSpawnAngularAcceleration;
+        public float   lineAngularVariation;
+        public float   lineSpawnAngularVariation;
+        public float   spawnAngle;
+        public float   spawnAngleVariation;
+        public int     boolArray;
+    }
+ 
     void GenerateDisplay()
     {
         var display     = new GameObject( "display" );
@@ -218,7 +297,7 @@ public class LinePlotMap : MonoBehaviour
         csErrosion.SetTexture( ClearID, "LineMap", lineMapTexture );
         csErrosion.Dispatch( ClearID, size.x / 8, size.y / 8, 1 );
 
-        int GetHeadsKID = csErrosion.FindKernel("GetHeads");
+        int GetHeadsKID = csErrosion.FindKernel(plotMethod.ToString());
         csErrosion.SetInts("Size", new int[] { size.x, size.y });
         csErrosion.SetInt("Frame", Time.frameCount);
         csErrosion.SetBuffer(GetHeadsKID, "HeadAppendBuffer", headBuffers[AppendBufferID]);
@@ -238,9 +317,11 @@ public class LinePlotMap : MonoBehaviour
         RenderTexture.active = rt;
         t2d.ReadPixels(new Rect(0,0,rt.width,rt.height),0,0);
         RenderTexture.active = null;
-        byte[] b = t2d.EncodeToPNG();
-        string p = String.Format( "{0}/{1}{2}.png", Application.persistentDataPath, this.name, DateTime.Now.GetHashCode() );
-        System.IO.File.WriteAllBytes(p,b);
+        byte[]  b = t2d.EncodeToPNG();
+        string  p = String.Format( "{0}/{1}{2}", Application.persistentDataPath, this.name, DateTime.Now.GetHashCode().ToString("X4") );
+        string ss = JsonUtility.ToJson(presentSaveState, true);
+        System.IO.File.WriteAllBytes(p+".png",b);
+        System.IO.File.WriteAllText(p+".json",ss);
         Debug.Log("Saved file: " + p);
     }
 }

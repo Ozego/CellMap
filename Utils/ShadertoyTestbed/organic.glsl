383fd7f9efb700a4e11bd32c8d4c219e6faf9824
Esben Jacob Sloth <esben@trueaxiongames.com>
2019-10-18 13:19:19 +0700

    better algorithm

--- Utils/ShadertoyTestbed/organic.glsl
+++ Utils/ShadertoyTestbed/organic.glsl
@@ -1,36 +1,27 @@
-vec2 fermatSpiralPosition(float t)
+#define THETA 2.399963229728653 //THETA is the golden angle in radians: 2 * PI * ( 1 - 1 / PHI )
+vec2 spiralPosition(float t)
 {
-    float theta = t * 2.399963229728653 - iTime*.001;
-    float radius = (t+.5)*.5;
-    // float radius = sqrt(t+.5)/.45;
-    return vec2(radius*cos(theta)+.5,radius*sin(theta)+.5);
+    float angle = t * THETA - iTime * .001; 
+    float radius = ( t + .5 ) * .5;
+    return vec2( radius * cos( angle ) + .5, radius * sin( angle ) + .5 );
 }
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord)
 {
-    int v = int( fragCoord.x ) & int( fragCoord.y );
     vec2 uv = ( fragCoord - .5 * iResolution.xy ) / iResolution.y * 1024.;
-    vec3 col = vec3( 0. );
     float a = 0.;
-    float d = 1.;
-    for(int i = 0; i < 200; i++)
+    float d = 50.;
+    for(int i = 0; i < 256; i++)
     {
-        vec2 point = fermatSpiralPosition(float(i))*6.66;
-        a += atan(uv.x-point.x,uv.y-point.y);
-        d = min(length(uv-point)*.02,d);
-        if(floor(point)!=floor(uv)) continue;
-        col = vec3(0.,1.,0.);
+        vec2 pointDist = uv - spiralPosition( float(i) ) * 6.66;
+        a += atan( pointDist.x, pointDist.y );
+        d = min( dot( pointDist, pointDist ), d );
     }
-    float r1 = 1.;
-    float r2 = 1.;
-    d = 1.-pow(1.-d,32.);
-    // a += sin(length(uv)*.01+iTime*.5)*2.75;
-    // a += length(uv)*pow(sin(iTime*.1),3.)*.5;
-    col   = smoothstep(.75,1.0,vec3(.5+.5*sin(a+iTime * -1. )));
-    col.r = (.5+.5*sin(a+iTime + 2.9 ));
-    col.g = (.5+.5*sin(a+iTime + 1.7 ));
-    col.b = (.5+.5*sin(a+iTime + 0.0 ));
-    // if(floor(uv)==vec2(0.))col=vec3(1.,0.,0.);
-    col   *= d;
+    d = sqrt( d ) * .02;
+    d = 1. - pow( 1. - d, 32. );
+    a += sin( length( uv ) * .01 + iTime * .5 ) * 2.75;
+    vec3 col  = d * (.5 + .5 * sin( a + iTime + vec3( 2.9, 1.7, 0 ) ) );
+    //col   = smoothstep( .75, 1.0, vec3( .5 + .5 * sin( a + iTime * -1. ) ) );
+    col *= d;
     fragColor = vec4( col, 1. );
 }
